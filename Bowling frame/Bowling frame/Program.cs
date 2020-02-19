9b2373252a3296404b5f92dfa3fbe35dfacb151a
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-02-19 13:12:13 +0000

    edited index

--- Bowling frame/Bowling frame/Program.cs
+++ Bowling frame/Bowling frame/Program.cs
@@ -1,3 +1,13 @@
 ﻿using System;
 
 namespace Bowling_frame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int throw1 = random.Next(0, 11);
+            int pinsLeft = (10 - throw1);
+            if (throw1 == 0)
+                    {
