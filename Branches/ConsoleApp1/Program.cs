3986f92cc903549ee61952cfda46fc3ea8b65e97
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-03-08 18:46:25 +0000

    fixed more typos

--- Branches/ConsoleApp1/Program.cs
+++ Branches/ConsoleApp1/Program.cs
@@ -11,4 +11,11 @@ namespace ConsoleApp1
             int c = 4;
             if (a + b + c > 10 && (a == b))
      {
-                Console.WriteLine("The answer is greater than 1
+                Console.WriteLine("The answer is greater than 10.");
+                Console.WriteLine("And the first number is equal to the second");
+        }
+        {
+            else
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+        
