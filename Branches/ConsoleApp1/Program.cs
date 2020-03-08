beabb7e5247e7e40290012b4e7072957ee957e51
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-03-08 10:44:46 +0000

    working...

--- Branches/ConsoleApp1/Program.cs
+++ Branches/ConsoleApp1/Program.cs
@@ -1,3 +1,14 @@
 ﻿using System;
 
-name
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+            int a = 5;
+            int b = 3;
+            int c = 4;
+            if (a + b + c > 10 && (a == b))
+     {
+                Console.WriteLine("The answer is greater than 1
