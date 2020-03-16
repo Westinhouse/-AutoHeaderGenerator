ce3af0df5698896941c418bc3c90333ff1521da3
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-03-16 20:57:36 +0000

    initialize the project

--- Branches/ConsoleApp2/Program.cs
+++ Branches/ConsoleApp2/Program.cs
@@ -34,4 +34,13 @@ namespace ConsoleApp2
             if ((a + b + c > 10) || (a > b))
             {
                 Console.WriteLine("The answer is greater than 10");
-                Console.WriteLine("Or the first number is 
+                Console.WriteLine("Or the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not greater than the second");
+            }
+        }
+    }
+    }
