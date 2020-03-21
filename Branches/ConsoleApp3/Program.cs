9ba5db4c6993f158ee3317f9a06106808ff5aa44
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-03-21 23:05:14 +0000

    A lot of stuff

--- Branches/ConsoleApp3/Program.cs
+++ Branches/ConsoleApp3/Program.cs
@@ -21,4 +21,13 @@ namespace ConsoleApp3
                 if (a + b > 10)
                 {
                     Console.WriteLine("The answer is greater than 10");
-              
+                }
+                else
+                {
+                    Console.WriteLine("The answer is not greater than 10");
+                }
+
+                int c = 4;
+                if ((a + b + c > 10) && (a > b))
+                {
+                    Console.WriteLine("The answer is gr
