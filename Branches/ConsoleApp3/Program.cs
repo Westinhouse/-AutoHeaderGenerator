45453707f92b7690adb42a365bd38a999e5d8d80
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-03-23 10:29:36 +0000

    Removed typo.

--- Branches/ConsoleApp3/Program.cs
+++ Branches/ConsoleApp3/Program.cs
@@ -42,4 +42,13 @@ namespace ConsoleApp3
                 if ((a + b + c > 10) || (a > b))
                 {
                     Console.WriteLine("The answer is greater than 10");
-                    Console.WriteLine("Or the first number is greater t
+                    Console.WriteLine("Or the first number is greater than the second");
+                }
+                else
+                {
+                    Console.WriteLine("The answer is not greater than 10");
+                    Console.WriteLine("And the first number is not greater than the second");
+                }
+            }
+
+           
