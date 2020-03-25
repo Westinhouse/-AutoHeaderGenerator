096ff079cb0bee164ce8f7d0ff58710d2acba3e5
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-03-25 20:45:24 +0000

    fuse: fix retrieve length

--- Branches/ConsoleApp3/Program.cs
+++ Branches/ConsoleApp3/Program.cs
@@ -51,4 +51,11 @@ namespace ConsoleApp3
                 }
             }
 
-           
+            void ExploreLoops()
+            {
+                int counter = 0;
+                while (counter < 10)
+                {
+                    Console.WriteLine($"Hello World! The counter is {counter}");
+                    counter++;
+             
