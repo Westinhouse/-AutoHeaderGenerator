57bdebef2997f1b9f253af4b4c5e3175adf873d5
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-03-29 19:33:12 +0000

    5.1.4

--- Branches/ConsoleApp3/Program.cs
+++ Branches/ConsoleApp3/Program.cs
@@ -93,4 +93,15 @@ namespace ConsoleApp3
             void ChallengeAnswer()
             {
                 int sum = 0;
-                for (int number = 1; number < 21; number
+                for (int number = 1; number < 21; number++)
+                {
+                    if (number % 3 == 0)
+                    {
+                        sum = sum + number;
+                    }
+                }
+                Console.WriteLine($"The sum is {sum}");
+            }
+        }
+    }
+}
