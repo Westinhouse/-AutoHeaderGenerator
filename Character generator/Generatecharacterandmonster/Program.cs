a84e8fa156424779a9b258503c6ead77f81f75f2
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-04-05 14:04:04 +0000

    Remove tabs.

--- Character generator/Generatecharacterandmonster/Program.cs
+++ Character generator/Generatecharacterandmonster/Program.cs
@@ -2,4 +2,15 @@
 
 namespace Generatecharacterandmonster
 {
-    interna
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+          
+            int roll(int diceSides)
+            {
+                return random.Next(1, diceSides + 1);
+            }
+
+            int strengh
