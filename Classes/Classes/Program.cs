da4ca9543bb6b585cae6d93059c581508c742625
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-04-12 19:25:26 +0000

    Update markdown files

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Classes
+{
+    internal class Program
+    {
+        class Location
+        {
+            public string Name;
+            public string Description;
+            public List<Location> Neighbors
