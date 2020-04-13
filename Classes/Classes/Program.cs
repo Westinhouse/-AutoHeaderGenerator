8ce80df1ee99fd280c5322e50a7b2aff2737cf92
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-04-13 07:07:18 +0000

    various corrections

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -10,4 +10,11 @@ namespace Classes
         {
             public string Name;
             public string Description;
-            public List<Location> Neighbors
+            public List<Location> Neighbors = new List<Location>();
+        }
+        static void ConnectLocations (Location a, Location b)
+        {
+           //for a, take location b and add it to the list neighbors for location a. Do the reverse for location b.
+           a.Neighbors.Add(b);
+           b.Neighbors.Add(a);
+      
