965dd77c859258de41686b933d248d573c439099
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-04-30 11:10:11 +0000

    chore: upgrade dependencies

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Randomize and print the amount of shots fired
+var random = new Random();
+int randomInteger = random.Next(10, 21);
+int totalShots = random.Next(10, 21);
+Console.WriteLine($"Total shots: {totalShots}");
+
