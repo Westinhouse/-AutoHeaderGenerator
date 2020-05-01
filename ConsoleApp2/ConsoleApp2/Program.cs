356ca404d53902b2aef595f795971ac85e824e37
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-05-01 08:42:58 +0000

    tidy

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,3 +4,11 @@ int randomInteger = random.Next(10, 21);
 int totalShots = random.Next(10, 21);
 Console.WriteLine($"Total shots: {totalShots}");
 
+// Randomize and print how many shots hit
+int hits = random.Next(totalShots);
+Console.WriteLine($"Hits: {hits}");
+
+// Calculate and print the hit percentage
+double acc = (double)hits / totalShots * 100;
+Console.WriteLine($"Accuracy {acc}% ");
+
