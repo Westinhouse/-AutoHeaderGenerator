7ec62e036c62c9f4d4198b4a8876c24ce739d619
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-05-27 15:38:59 +0000

    init README

--- Lists/Lists mission 1/Program.cs
+++ Lists/Lists mission 1/Program.cs
@@ -10,4 +10,10 @@ namespace Lists_mission_1
             {
                 var random = new Random();
 
-                int
+                int roll(int diceSides)
+                {
+                    return random.Next(1, diceSides + 1);
+                }
+                var abilityScores = new List<int>();
+                //To roll ability scores you roll 4d6 and discard the lowest
+                // Lets do this by rolling 4d6, sorting them from lowest to highest 
