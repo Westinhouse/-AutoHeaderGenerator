17254ec9982cb91022661ab4f45333d01ecd04da
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-05-27 10:25:51 +0000

    enhance

---
+++ Lists/Lists mission 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists_mission_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var random = new Random();
+
+                int
