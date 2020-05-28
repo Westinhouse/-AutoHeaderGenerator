bc980bb0a2a3ab5d8f8335267a92cb975d404744
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-05-28 05:54:30 +0000

    Added missing javadoc

--- Lists/Lists mission 1/Program.cs
+++ Lists/Lists mission 1/Program.cs
@@ -16,4 +16,11 @@ namespace Lists_mission_1
                 }
                 var abilityScores = new List<int>();
                 //To roll ability scores you roll 4d6 and discard the lowest
-                // Lets do this by rolling 4d6, sorting them from lowest to highest 
+                // Lets do this by rolling 4d6, sorting them from lowest to highest and then summing up the rolls, skipping the first index.
+                // lets do that 6 times
+                for (int i = 0; i < 6; i++)
+                {
+                    var rolls = new List<int>();
+
+                    for (int j = 0; j < 4; j++)
+        
