e54bd8837fd68c8e49dd6cc752832cebd3f1c7b6
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-06-04 10:21:21 +0000

    Day 21

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -18,4 +18,12 @@ namespace fundamentals_lists
                 }
 
                 Console.WriteLine();
-       
+                names.Add("Maria");
+                names.Add("Bill");
+                names.Remove("Ana");
+                foreach (var name in names)
+                {
+                    Console.WriteLine($"Hello {name.ToUpper()}!");
+                }
+
+                
