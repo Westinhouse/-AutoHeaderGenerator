214faea20a8bb9fbc5b0afb3f8232b44b9e0d17a
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-06-04 14:11:09 +0000

    Fix typo in error message

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -26,4 +26,10 @@ namespace fundamentals_lists
                     Console.WriteLine($"Hello {name.ToUpper()}!");
                 }
 
-                
+                Console.WriteLine($"My name is {names[0]}");
+                Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+                Console.WriteLine($"The list has {names.Count} people in it");
+
+                var index = names.IndexOf("Felipe");
+                if (index == 
