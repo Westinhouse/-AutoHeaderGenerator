e98bebf8c11729bf50d9860c9ab45823ba5a63dd
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-06-05 13:06:23 +0000

    Fix argument name

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -41,4 +41,11 @@ namespace fundamentals_lists
                     Console.WriteLine($"The name {names[index]} is at index {index}");
                 }
 
-                i
+                index = names.IndexOf("Not Found");
+                if (index == -1)
+                {
+                    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+                }
+                else
+                {
+                    Console.WriteLine($"The name {names[index]}
