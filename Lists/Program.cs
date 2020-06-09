0f5b881d74b17c15b5572013b4ad180286394124
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-06-09 08:39:07 +0000

    New files added

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -58,4 +58,12 @@ namespace fundamentals_lists
                     Console.WriteLine($"Hello {name.ToUpper()}!");
                 }
 
-                var fibonacciNumbers =
+                var fibonacciNumbers = new List<int> { 1, 1 };
+              
+
+                while (fibonacciNumbers.Count < 20)
+                {
+                    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                    fibonacciNumber
