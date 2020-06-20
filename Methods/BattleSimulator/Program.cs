7dd6e5f80c6e41c866586d4ea135ea83bbab3772
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-06-20 07:26:03 +0000

    Testing data generated

---
+++ Methods/BattleSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BattleSimulator
+{
+    internal class Program
+    {
+        static int DiceRoll(int numberOfRolls, int diceSides, int fixedBonus = 0)
+        {
+            var random = new Random();
+            int sum = 0;
+      
