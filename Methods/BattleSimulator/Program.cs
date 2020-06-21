c626832474785c8bef8084e7f8e6a709be756966
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-06-21 09:35:17 +0000

    Include fix

--- Methods/BattleSimulator/Program.cs
+++ Methods/BattleSimulator/Program.cs
@@ -9,4 +9,12 @@ namespace BattleSimulator
         {
             var random = new Random();
             int sum = 0;
-      
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                sum += random.Next(1, diceSides + 1);
+            }
+             return sum += fixedBonus;
+
+        }
+
+        static void SimulateBattle(List<string> heroNames, string monsterName, int monsterHP, i
