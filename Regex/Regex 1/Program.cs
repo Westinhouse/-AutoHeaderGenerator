62b86ab10ef6023251626e91a5b8a53f388180c9
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-07-20 14:40:50 +0000

    Use strict

--- Regex/Regex 1/Program.cs
+++ Regex/Regex 1/Program.cs
@@ -1,2 +1,16 @@
 ﻿using System;
-using System.Collections.G
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Regex_1
+{
+    internal class Program
+    {
+
+        static List<string> monsterNames = new List<string>();
+
+        static void Main(string[] args)
+        {
+
+            string[] monsterManual = File.Read
