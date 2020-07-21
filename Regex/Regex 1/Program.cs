84ff9b21428da5ee0c182816fa63c788b9967801
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2020-07-21 05:48:27 +0000

    Translated using Weblate (Norwegian Bokmål (nb))

--- Regex/Regex 1/Program.cs
+++ Regex/Regex 1/Program.cs
@@ -13,4 +13,14 @@ namespace Regex_1
         static void Main(string[] args)
         {
 
-            string[] monsterManual = File.Read
+            string[] monsterManual = File.ReadAllLines("MonsterManual.txt");
+            monsterNames.Add(monsterManual[0]);
+            List<bool> CanFly = new List<bool>();
+            List<bool> IsTenDiceOrMore = new List<bool>();
+
+
+            for (int i = 0; i < monsterManual.Length; i++)
+            {
+
+                if (monsterManual[i] == "")
+      
