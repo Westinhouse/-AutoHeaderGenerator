0cce4a1545ebf86534f814481bf881b31e277510
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2021-03-06 10:04:20 +0000

    Update changelog_release.xml

--- Regex/Regex 3  - 1/Program.cs
+++ Regex/Regex 3  - 1/Program.cs
@@ -1,3 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using Sy
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Regex_3____1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] monsterManualLines = File.ReadAllLines("MonsterManual.txt");
+            string monsterManual = File.ReadAllText("
