0aa8cae71f30e8fccd804120bb6e5be7cef0a074
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2021-04-03 15:21:51 +0000

    update file

--- Regex/Regex 4/Program.cs
+++ Regex/Regex 4/Program.cs
@@ -9,4 +9,9 @@ namespace Regex_4
         static void Main(string[] args)
         {
             var httpClient = new HttpClient();
-            string[] games = { "https://store.steampowered.com/app/553420/TUNIC/", "https://store.steampowered.com/app/1657630/Slime_Rancher_2/", "https://store.steampowered.com/app
+            string[] games = { "https://store.steampowered.com/app/553420/TUNIC/", "https://store.steampowered.com/app/1657630/Slime_Rancher_2/", "https://store.steampowered.com/app/1811260/EA_SPORTS_FIFA_23/", "https://store.steampowered.com/app/1954200/Kena_Bridge_of_Spirits/" };
+
+            for (int i = 0; i < games.Length; i++)
+            {
+                string htmlCode = httpClient.GetStringAsync(games[i]).Result;
+                Match name = Rege
