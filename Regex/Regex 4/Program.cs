7321978b76eb34746710877d46aad1c59741d7f1
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2021-04-03 12:20:25 +0000

    upgrading dependencies

--- Regex/Regex 4/Program.cs
+++ Regex/Regex 4/Program.cs
@@ -4,4 +4,9 @@ using System.Text.RegularExpressions;
 
 namespace Regex_4
 {
-    internal
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            string[] games = { "https://store.steampowered.com/app/553420/TUNIC/", "https://store.steampowered.com/app/1657630/Slime_Rancher_2/", "https://store.steampowered.com/app
