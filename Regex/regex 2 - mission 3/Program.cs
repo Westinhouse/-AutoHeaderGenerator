dc84fefb36ca2037cd3312cd17ba971545cec105
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2022-09-30 05:30:05 +0000

    Update subscribe.html

--- Regex/regex 2 - mission 3/Program.cs
+++ Regex/regex 2 - mission 3/Program.cs
@@ -12,3 +12,10 @@ namespace regex_2___mission_3
         static void Main(string[] args)
         {
             //Setting up the list of strings that is the monster manual
+            string[] monsterManual = File.ReadAllLines("MonsterManual.txt");
+
+            //Initalizing the list that will store the alignments
+            List<string> alignment = new List<string>();
+
+            //Setting which character we will split the string at later
+            char[] deli
