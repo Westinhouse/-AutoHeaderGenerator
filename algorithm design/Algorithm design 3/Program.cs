63b9d4321b48e301c154df99d903f58fcd15a861
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2022-10-08 10:23:48 +0000

    atl1c: Improve driver not to do order 4 GFP_ATOMIC allocation

--- algorithm design/Algorithm design 3/Program.cs
+++ algorithm design/Algorithm design 3/Program.cs
@@ -85,3 +85,215 @@ namespace Algorithm_design_3
 
             int currentCurveX = position;
             var curveValues = new List<int>();
+
+            for (int y = 0; y < height; y++)
+            {
+                CurveDirection direction = GetRandomDirection(curveChance);
+
+                if (direction != CurveDirection.Straight)
+                {
+                    if (direction == CurveDirection.Right)
+                    {
+                        ++currentCurveX;
+                    }
+                    else
+                    {
+                        --currentCurveX;
+                    }
+                }
+
+                curveValues.Add(currentCurveX);
+
+            }
+            return curveValues;
+
+        }
+        static void CreateMap()
+        {
+
+            // Generating Wall
+            wallXPositionsList = GenerateCurve(width * 1 / 4, 12);
+
+            // Generating the river
+            riverXPositionsList = GenerateCurve(width * 3 / 4, 3);
+
+
+            // Generating the road that goes left to right
+            int roadStartY = height / 2;
+            int currentRoadY = roadStartY;
+            for (int x = 0; x < width; x++)
+            {
+
+                /* This section checks if the current position is close to another map element,
+                 * and if so, set a boolean to tell the later section that this position is close to another map element*/
+                bool isCloseToMapElement = false;
+
+                for (int n = -3; n <= 5; n++)
+                {
+                    if (riverXPositionsList.Contains(x + n) || wallXPositionsList.Contains(x + n))
+                    {
+                        isCloseToMapElement = true;
+                        break;
+                    }
+                }
+
+                CurveDirection roadDirection = GetRandomDirection(8);
+
+                if (roadDirection != CurveDirection.Straight && !isCloseToMapElement) // if we are not close to an object, the road can curve.
+                {
+                    if (roadDirection == CurveDirection.Right)
+                    {
+                        ++currentRoadY;
+                    }
+                    else
+                    {
+                        --currentRoadY;
+                    }
+                }
+
+                roadLeftRightYPositionsList.Add(currentRoadY);
+            }
+            return;
+        }
+
+        static void DrawMap()
+        {
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    /*Drawing the borders by checking if the current position "x" is equal to 0 or the width. 
+                     * similar with "y", is it 0 or the height?*/
+
+                    if (x == 0 && y == 0 || x == width - 1 && y == height - 1 || x == width - 1 && y == 0 || x == 0 && y == height - 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("+");
+                        continue;
+                    }
+                    else if (x == 0 || x == width - 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("|");
+                        continue;
+                    }
+                    else if (y == 0 || y == height - 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("-");
+                        continue;
+                    }
+
+                    /* Positioning and Drawing the title, and making sure that the title doesnt "push" 
+                     * the map positions weirdly by moving the current x position in the algorithm
+                     * forward based on the lenght of the string*/
+
+                    if (x == width / 2 - mapTitle.Length / 2 && y == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(mapTitle);
+                        x += mapTitle.Length - 1;
+                        continue;
+                    }
+
+                    // Drawing the bridge, we check if the road and river are intersecting, or will intersect in the coming coordinates.
+                    if (x >= riverXPositionsList[y] - 1 && x <= riverXPositionsList[y] + 3 && y + 1 == roadLeftRightYPositionsList[x] || x >= riverXPositionsList[y] - 1 && x <= riverXPositionsList[y] + 3 && y - 1 == roadLeftRightYPositionsList[x])
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("=");
+                        continue;
+                    }
+
+                    //Drawing the road that leads down by checking if we are 5 steps before from the river and past the road on the y axis.
+                    if (x == riverXPositionsList[y] - 5 && y > roadLeftRightYPositionsList[x])
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("#");
+                        continue;
+                    }
+
+                    //Drawing the road that goes left to right
+                    if (roadLeftRightYPositionsList[x] == y)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("#");
+                        continue;
+                    }
+
+                    //Drawing the turrets, using a similar technique as when drawing the title to ensure correct positioning.
+                    if (x == wallXPositionsList[y] && y + 1 == roadLeftRightYPositionsList[x] || x == wallXPositionsList[y] && y - 1 == roadLeftRightYPositionsList[x])
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write(turretShape);
+                        x += turretShape.Length - 1;
+                        continue;
+                    }
+
+                    //Drawing the wall
+                    if (x >= wallXPositionsList[y] && x <= wallXPositionsList[y] + 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        DrawCurve(wallXPositionsList, y);
+                        continue;
+                    }
+
+
+                    // Drawing the trees
+                    double treeDensity = (double)-x / (width / 4) + 1;
+                    double treeRoll = random.NextDouble();
+                    if (treeRoll < treeDensity)
+                    {
+                        int treeColor = random.Next(2);
+
+                        if (treeColor == 1)
+                        {
+                            Console.ForegroundColor = treeConsoleColor;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = darkTreeConsoleColor;
+                        }
+                        Console.Write($"{treeSymbolsList[random.Next(treeSymbolsList.Count)]}");
+                        continue;
+                    }
+
+
+                    //Drawing the river
+                    if (x >= riverXPositionsList[y] && x <= riverXPositionsList[y] + 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        DrawCurve(riverXPositionsList, y);
+                        continue;
+                    }
+
+                    // Drawing mountains
+                    int mountain = random.Next(x);
+                    if (mountain > width / 2 && y < roadLeftRightYPositionsList[x])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("^");
+                        continue;
+                    }
+
+
+
+
+                    //Filling in space thats not drawn with blanks
+                    Console.Write(" ");
+
+
+                }
+                Console.WriteLine(); //ensuring line breaks at the end of the x-axis
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            CreateMap();
+            DrawMap();
+        }
+    }
+}
+
+
