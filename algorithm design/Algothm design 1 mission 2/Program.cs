a4258a36c8cee0837b8cf1dbe308454ba86158e5
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2022-10-10 22:45:04 +0000

    Primeiro commit

--- algorithm design/Algothm design 1 mission 2/Program.cs
+++ algorithm design/Algothm design 1 mission 2/Program.cs
@@ -5,4 +5,17 @@ namespace Algothm_design_1_mission_2
 {
     internal class Program
     {
-        static string JoinWithAnd(List<string> heroes, bool commaFlag 
+        static string JoinWithAnd(List<string> heroes, bool commaFlag = false)
+        {
+            //count the number of heronames in list
+            int count = heroes.Count;
+
+            if (count == 0)
+            {
+                return (" ");
+            }
+
+            else if (count == 1)
+            {
+                return($"{heroes[0]}");
+         
