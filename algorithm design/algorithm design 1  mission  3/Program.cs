5ff21079cae040cfd734bbe2c5b7e5e51b5f29e7
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2022-11-02 18:33:47 +0000

    android: base-cfg: disable ALARM_DEV

--- algorithm design/algorithm design 1  mission  3/Program.cs
+++ algorithm design/algorithm design 1  mission  3/Program.cs
@@ -1,1 +1,13 @@
-﻿using
+﻿using System;
+
+namespace algorithm_design_1__mission__3
+{
+    internal class Program
+    {
+        static string OrdinalNumber(int number)
+        {
+            int lastDigit = number % 10;
+            int secondToLastDigit = number / 10 % 10;
+
+            if (secondToLastDigit == 1)
+   
