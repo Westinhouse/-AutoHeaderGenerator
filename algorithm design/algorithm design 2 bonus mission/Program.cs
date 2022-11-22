8429ddae3eed29145ad451d2d1c9534152431fef
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2022-11-22 14:49:10 +0000

    Update authors list

--- algorithm design/algorithm design 2 bonus mission/Program.cs
+++ algorithm design/algorithm design 2 bonus mission/Program.cs
@@ -16,4 +16,12 @@ namespace algorithm_design_2_bonus_mission
         static IList<IList<int>> Permute(int[] nums)
         {
             var list = new List<IList<int>>();
-        
+            return DoPermute(nums, 0, nums.Length - 1, list);
+        }
+
+        static IList<IList<int>> DoPermute(int[] nums, int start, int end, IList<IList<int>> list)
+        {
+            if (start == end)
+            {
+                // We have one of our possible n! solutions,
+                //
