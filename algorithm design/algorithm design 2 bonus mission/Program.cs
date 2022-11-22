55a7cf495627158dd0d036e2fef49248c9b391be
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2022-11-22 10:25:42 +0000

    Update about me page

--- algorithm design/algorithm design 2 bonus mission/Program.cs
+++ algorithm design/algorithm design 2 bonus mission/Program.cs
@@ -3,4 +3,17 @@ using System.Collections.Generic;
 
 namespace algorithm_design_2_bonus_mission
 {
-    internal class Pro
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            PrintResult(
+                Permute(new int[] { 1, 2, 3 })
+            );
+        }
+
+        static IList<IList<int>> Permute(int[] nums)
+        {
+            var list = new List<IList<int>>();
+        
