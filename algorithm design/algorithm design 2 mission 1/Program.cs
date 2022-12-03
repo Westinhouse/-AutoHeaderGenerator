8da592ae6dce096b777b098b03d49fbd9e5e7c9e
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2022-12-03 21:36:17 +0000

    Removed unused parameters

--- algorithm design/algorithm design 2 mission 1/Program.cs
+++ algorithm design/algorithm design 2 mission 1/Program.cs
@@ -1,2 +1,13 @@
 ﻿using System;
-using System.C
+using System.Collections.Generic;
+
+namespace algorithm_design_2_mission_1
+{
+    internal class Program
+    {
+        static string ShuffleList(List<string> names)
+        {
+            var random = new Random();
+            for (int i = names.Count - 1; i > 0; i--)
+            {
+                int k = ra
