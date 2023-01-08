097b79463164594bb8176d895bc60981157a4090
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2023-01-08 11:19:31 +0000

    remove

--- arrays/Arrays boss level part 1/Program.cs
+++ arrays/Arrays boss level part 1/Program.cs
@@ -4,4 +4,9 @@ namespace Arrays_boss_level_part_1
 {
     internal class Program
     {
-        static void Main(string[] a
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
