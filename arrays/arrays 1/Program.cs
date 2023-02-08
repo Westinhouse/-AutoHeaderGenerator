3343844f8e9c5cd2bc53538671406abb9b94d5aa
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2023-02-08 11:28:49 +0000

    Fixes indentation.

--- arrays/arrays 1/Program.cs
+++ arrays/arrays 1/Program.cs
@@ -1,1 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+
+namespace arrays_1
+{
+    internal class Program
+    {
+        static void CreateDayDescription(int day, int season, int year)
+        {
+            string[] days = {"1", "2", "3", "4"};
+            string[] seasons = {"Spring", "Summer",
