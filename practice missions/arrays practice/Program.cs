8c57dbf4b624b5013726e04dd54a9935b694a9cc
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2023-03-12 07:59:42 +0000

    Updated TODOs.

---
+++ practice missions/arrays practice/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace arrays_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            //Pri
