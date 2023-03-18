14bde4bc1d2142e227e30703ac34a5b82eb25264
Westinhouse <119607988+Westinhouse@users.noreply.github.com>
2023-03-18 06:19:23 +0000

    chore(package): update coveralls to version 2.11.14

--- practice missions/arrays practice/Program.cs
+++ practice missions/arrays practice/Program.cs
@@ -13,4 +13,13 @@ namespace arrays_practice
             {
                 Console.WriteLine(day);
             }
-            string[] dayNumbers = new string[30] {"1: Thursday ", "2: Friday ", "3: Saturday ", "4: Sunday ", "5: Monday ", "6: Tuesday ", "7: Wednesday ", "8: Thursday ", "9: Friday ", "10: Saturday ", "11: Sunday ", "12: Monday ", "13: Tuesday ", "14: Wednesday ", "15: Thursday ", "16: Friday ", "17: Saturday ", "18: Sunday ", "19: Monday ", "20: Tuesday ", "21: Wednesday ", "22: Thursday ", "23: Friday ", "24: Saturday ", "25: Sunday ", "
+            string[] dayNumbers = new string[30] {"1: Thursday ", "2: Friday ", "3: Saturday ", "4: Sunday ", "5: Monday ", "6: Tuesday ", "7: Wednesday ", "8: Thursday ", "9: Friday ", "10: Saturday ", "11: Sunday ", "12: Monday ", "13: Tuesday ", "14: Wednesday ", "15: Thursday ", "16: Friday ", "17: Saturday ", "18: Sunday ", "19: Monday ", "20: Tuesday ", "21: Wednesday ", "22: Thursday ", "23: Friday ", "24: Saturday ", "25: Sunday ", "26: Monday ", "27: Tuesday ", "28: Wednesday ", "29: Thursday ", "30: Friday " };
+
+            //print the months days and numbers
+
+            //for (int i = 0; i < dayNumbers.Length; i++)
+            //{
+            //    Console.Write(dayNumbers[i]);
+            //}
+
+           
